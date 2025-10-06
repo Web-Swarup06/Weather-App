942baac8604ad7d123a371d007ec18e695065c52
Web-Swarup06 <swarupsamaddarkgp@gmail.com>
2025-10-06 12:58:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import requests
+
+#API KEY
+api="a1b6bf1a284e457f7f59dd485b2b072e"
+
+#GET DATA FROM OPEN WEATHER WEBSITE
+def place_name(name):
+    url=f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={api}&units=metric"
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        weather_data = r.json()
+        return weather_data
+    
+#PAGE SETTINGS
+st.set_page_config(page_title="Weather Forecast", page_icon="🌤️", layout="centered")
+
+#TITLE
+st.title("🌦️ Weather Forecasting App")
+
+#CITY NAME INPUT
+st.subheader('Type the city name')
+name = st.text_input('',placeholder="e.g. Kolkata, London, Tokyo")
+
+if st.button('Get Weather'):
+    w_info = place_name(name)
+
+    if w_info:
+        st.metric(label="📍 City Name", value=f"{w_info['name']}")
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.metric(label="🌍 Longitude", value=f"{w_info['coord']['lon']}")
+            
+        with col2:
+            st.metric(label="🧭 Latitude", value=f"{w_info['coord']['lat']}")
+
+        with col3:
+            st.metric(label="🌡️ Temperature", value=f"{w_info['main']['temp']}")
+
+            
+
